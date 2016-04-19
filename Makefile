99ab00ad7b80f28aef6595cbabcc02b82a6f5eb0
Martin maga <martin@a05-0517a.kn.vutbr.cz>
2016-04-19 23:39:21 +0200

    initia commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+#xmagam00
+#KRY 2
+build:
+	g++ -lgmp -Wall kry.cpp -o kry
+pack:
+	zip xmagam00.zip kry.cpp Makefile
+clean:
+	rm xmagam00.zip kry
---
+++ rsa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstddef>
+#include <gmp.h>
+
+
+using namespace std;
+int main(int argc, char **argv)
+{
+cout<<arv[0]<<endl;
+return 0;
+}
